545478599f8b76e95cbe530f46f52627bf42e3f4
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-24 23:08:18 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("APP_ID", "15260804"))
+    API_HASH = os.environ.get("API_HASH", "e5ff231de8c0ee33fa7830e766df5b10")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5332418768:AAFObHqPv72dyAG6vdnSPDX5ylclXSuXdpE")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKsBuzRlVHX2kqehclC58cV6aizURdaxHlLk5xdMZ-b2Ewk7nU-7b8qVBrp1txA0fjhNH1bxpG0qluYVy4RWxHARHXeKbRKPG0pnGC2CBJQUIJDx4ya_dv2PK93bZIw4J-qT1ZxtoxTOBUygc-lrEJx9f9M74wTgUxUn-HUSUKXxzIEkSMaVYZR-M86R7plpcMkyomw1JuHCXCtf_3waAxLUdBd3TxVBP5Tj7RXmib32T-IzU8o-jGJg32kS-b6Mo1nqgPOPvszRZPhgJg4H5rtdPAMVr-zknjDTsmHs9ToS6aHFI34VycQe6kx6aT1v4IBFVXTAK0Wr1fDc-tHevJyaFko=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Zaid2_Robot")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "LOVE_PRO_VIKAS_OP_bot")
+    SUPPORT = os.environ.get("SUPPORT", "LOVE_IS_LIFE_OP")
+    CHANNEL = os.environ.get("CHANNEL", "OFFICIALHACKER789")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
